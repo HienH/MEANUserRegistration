3022d4d51e6fa2d8e41c5eb1b27e12497edc50c2
hien hang <hienhang@hotmail.co.uk>
2020-10-11 17:42:52 +0100

    rebuilding heroku- changed db values

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,6 +1,7 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user.model');
+
 // use env config
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
